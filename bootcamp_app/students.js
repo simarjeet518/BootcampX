62a8dc90295e6c44e2958e8036c5974a11422491
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-01-06 04:39:22 +0000

    parametarized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,15 +9,15 @@ const Config ={
 const pool = new Pool(Config);
 
 const cohort = process.argv[2];
-const limit = process.argv[3];
-
+const limit = process.argv[3] || 5;
+const values =[`%${cohort}%`,limit];
 pool.query(`
 SELECT students.id, students.name , cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-where cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit || 5};
-`)
+where cohorts.name LIKE $1
+LIMIT $2;
+`,values)
 .then(res => {
   res.rows.forEach(student => {
     console.log(`${student.name} has student id ${student.id} was  in cohort  ${student.cohort}`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,7 +9,7 @@ const Config ={
 const pool = new Pool(Config);
 
 const cohort = process.argv[2];
-
+const values =[`%${cohort}%`];
 
 pool.query(`
 SELECT distinct teachers.name as teacher, cohorts.name as cohort ,COUNT(assistance_requests.*) as total_assistances
@@ -17,10 +17,10 @@ FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id 
 JOIN  students ON student_id = students.id 
 JOIN cohorts ON students.cohort_id = cohorts.id 
-WHERE cohorts.name LIKE '%${cohort}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name , cohorts.name
 ORDER BY teacher;
-`)
+`,values)
 .then(res => {
   res.rows.forEach(data => {
     console.log(`${data.cohort} : ${data.teacher} `);
